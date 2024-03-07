86753204d1605ab109363b08e50a972888cd2b0a
penekhun <penekhun@gmail.com>
2024-03-07 14:45:17 +0900

    test: Develop user options #38

---
+++ easypeel-method-security/src/test/java/org/easypeelsecurity/configuration/LogOptionsTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.easypeelsecurity.configuration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class LogOptionsTest {
+
+  @ParameterizedTest
+  @CsvSource({"true", "false"})
+  void enableSetterAndGetter(boolean enabled) {
+    // given
+    final LogOptions logOptions = new LogOptions();
+
+    // when
+    logOptions.setEnabled(enabled);
+
+    // then
+    assertEquals(enabled, logOptions.isEnabled());
+  }
+
+  @ParameterizedTest
+  @CsvSource({"DEV", "PROD"})
+  void logLevelSetterAndGetter(LogLevel level) {
+    // given
+    final LogOptions logOptions = new LogOptions();
+
+    // when
+    logOptions.setLevel(level);
+
+    // then
+    assertEquals(level, logOptions.getLevel());
+  }
+}
